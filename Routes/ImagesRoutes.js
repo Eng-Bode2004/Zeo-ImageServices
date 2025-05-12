769db3ef426e559d1cc8fb0441924e3af38f15e7
Abdelrahman Ashraf <bodeashrafahmed@gmail.com>
2025-05-12 03:50:26 +0300

    Rule Image

--- Routes/ImagesRoutes.js
+++ Routes/ImagesRoutes.js
@@ -4,9 +4,14 @@ const ImagesControllers = require('../Controllers/ImagesControllers');
 
 
 
-
+                            // Upload User Profile Image //
 
 const upload = require('../Middlewares/Multer-Users-ProfileImage'); // parser
 router.post('/profile-image', upload.single('image'), ImagesControllers.uploadPhoto);
+
+                             // Upload Rules Images//
+const Rule = require('../Middlewares/Multer-Rules-Images'); // parser
+router.post('/Rule-image', Rule.single('image'), ImagesControllers.uploadPhoto);
+
 
 module.exports = router;
